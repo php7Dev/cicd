248101107c2863805220d483aef2f0b50d52a373
unknown <fikra.sys@gmail.com>
2025-05-03 18:58:37 +0300

    [Docs] Add some explainations.

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
 
 echo 'Hi there Nagd';
+
+/*
+
+git init | git clone << repo url >>
+
+git remote -v
+git remote add origin << repo path ssh | https >>
+
+git add << file name | .(files) >>
+git commit -m "<< message >>"
+
+git status
+
+git push origin << -f | -u >> <<  branch name >>
+
+
+git checkout -b "<< branch name>>"  
+
++
+
+git branch << branch name>>
+git switch "<< branch name >>"
+
+*/
